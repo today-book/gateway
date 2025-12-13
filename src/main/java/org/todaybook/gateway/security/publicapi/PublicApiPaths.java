14bfd2e613ad9dc68d54cdf4a1d2ff01ab6a6023
JKjiwon <jwkim.oa@gmail.com>
2025-12-13 12:51:37 +0900

    [#4] Build: 배포 준비

--- src/main/java/org/todaybook/gateway/security/publicapi/PublicApiPaths.java
+++ src/main/java/org/todaybook/gateway/security/publicapi/PublicApiPaths.java
@@ -5,8 +5,8 @@ import java.util.List;
 public final class PublicApiPaths {
   public static final List<String> PATHS =
       List.of(
-          "/api/v1/search/books",
           "/public/**",
+          "/api/v1/search/books",
           "/api/v1/auth/login",
           "/api/v1/auth/logout",
           "/api/v1/auth/refresh");
